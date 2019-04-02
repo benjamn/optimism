2ce9f3deb2ea75ba64f74073c55f1e4313c3501a
Ben Newman <ben@benjamn.com>
2019-04-02 13:33:54 -0400

    Add a test of cached sorting performance.

--- src/tests/performance.ts
+++ src/tests/performance.ts
@@ -1,4 +1,5 @@
-import { wrap } from "../index";
+import * as assert from "assert";
+import { wrap, KeyTrie } from "../index";
 
 describe("performance", function () {
   this.timeout(30000);
@@ -14,5 +15,55 @@ describe("performance", function () {
     for (let i = 0; i < 100000; ++i) {
       parent({}, i, {});
     }
+  });
+
+  it("can speed up sorting with O(array.length) cache lookup", function () {
+    let counter = 0;
+    const trie = new KeyTrie(false);
+    const sort = wrap((array: number[]) => {
+      ++counter;
+      return array.slice(0).sort();
+    }, {
+      makeCacheKey(array) {
+        return trie.lookupArray(array);
+      }
+    });
+
+    assert.deepEqual(sort([2, 1, 5, 4]), [1, 2, 4, 5]);
+    assert.strictEqual(counter, 1);
+    assert.strictEqual(
+      sort([2, 1, 5, 4]),
+      sort([2, 1, 5, 4]),
+    );
+    assert.strictEqual(counter, 1);
+
+    assert.deepEqual(sort([3, 2, 1]), [1, 2, 3]);
+    assert.strictEqual(counter, 2);
+
+    const bigArray: number[] = [];
+    for (let i = 0; i < 10000; ++i) {
+      bigArray.push(Math.round(Math.random() * 100));
+    }
+
+    const bigArrayCopy = bigArray.slice(0);
+    const rawSortStartTime = Date.now();
+    bigArrayCopy.sort();
+    const rawSortTime = Date.now() - rawSortStartTime;
+
+    assert.deepEqual(
+      sort(bigArray),
+      bigArrayCopy,
+    );
+
+    const cachedSortStartTime = Date.now();
+    const cached = sort(bigArray);
+    const cachedSortTime = Date.now() - cachedSortStartTime;
+
+    assert.deepEqual(cached, bigArrayCopy);
+    assert.ok(
+      cachedSortTime <= rawSortTime,
+      `cached: ${cachedSortTime}ms, raw: ${rawSortTime}ms`,
+    );
+    assert.strictEqual(counter, 3);
   });
 });
